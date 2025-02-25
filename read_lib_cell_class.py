0dc5a26851acfcf9a6f3b678e1888bef3ba59573
jemeu <104530777+jemeu@users.noreply.github.com>
2025-02-25 12:22:28 +0000

    Completed parse_header method

--- read_lib_cell_class.py
+++ read_lib_cell_class.py
@@ -1,34 +1,61 @@
 import re
 
 
-class LibProcessor():
+# The plan here is to maybe set a chunk size of maybe 100 lines
+
+class Library:
+    def __init__(self):
+        pass
+            
+class Cell(Library):
+    def __init__(self):
+        pass 
 
 
-    def __init__(self, file_path:str, cell_name):
+class Pin(Cell):
+    def __init__(self):
+        pass    
+
+
+# Have a separate class for parsing the file from the library itself, don't know how to jfjfjfj
+class LibertyParser:
+    def __init__(self, file_path:str):
         self.file_path = file_path
-        self.cell_name = cell_name
+
+        # Don't know whether this should be a string or a list
+        self.header_text = ""
+
+        self.file = open(self.file_path,'r')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        pass
     
+    def parse_header(self,output_path = None):
+        '''
+        Either returns the header of a .lib file, or writes it to a user specified location using output_path
+        '''
+        for line in self.file:
+            # If not the first cell
+            if not re.search('\s*cell\s*\(',line):
+                self.header_text += line
+            else:
+                break
+        # Removes any extra blank lines at the end of the file
+        # (as blank lines will still have a newline character at the end, resulting in two lines being printed)                   
+        if self.header_text.endswith("\n"):
+            self.header_text = self.header_text[:-1]
 
-    def process_lib(self):
-        with open(self.file_path,'r') as file:
-            lines_printed = 0
-            printing = True
+        # Allows for an optional argument that specifies where to store it
+        if output_path:
+            with open(output_path,"w") as file:
+                file.write(self.header_text)
+        else:
+            return self.header_text
 
-            for line in file:
-                # Prints the header
-                # Checks if there is 'cell' followed by one or more 'anything other than a new line', then an open bracket
-                if re.search('cell\s*\(',line):
-                    printing = False
+myFile = LibertyParser('example.lib')
+myFile.parse_header()
 
-                
-
-                # Keeps track of number of lines printed
-                if printing == True:
-                    print(line)
-                    lines_printed += 1
-
-            print(f"Lines printed = {lines_printed}")
-
-
-myprocessor = LibProcessor("example.lib","INV_X8N_A9PP96CTL_C20")
-myprocessor.process_lib()
+    
