3db4554e5e1a7a795d712e6e7d23d76262d1e8bc
jemeu <104530777+jemeu@users.noreply.github.com>
2025-02-26 12:19:23 +0000

    WORKING Redefined Library as subclass of Group class

--- read_lib_cell_class.py
+++ read_lib_cell_class.py
@@ -3,30 +3,16 @@ import re
 
 # The plan here is to maybe set a chunk size of maybe 100 lines
 
-class Library:
-    def __init__(self,definition = None,name = None):
-        self.definition = definition
-        self.name = name
-
-    # This is a test method to check if something is actually an object or not
-    # REMEMBER THIS IS A TEST METHOD 
-    def explode(self):
-        print("EXPLODED")
-
-class Cell(Library):
+class Group:
     def __init__(self):
-        pass 
-
-
-class Pin(Cell):
-    def __init__(self):
-        pass    
-
+        self.definition = None
 
 # Have a separate class for parsing the file from the library itself, don't know how to jfjfjfj
-class LibertyParser:
-    def __init__(self, lib_path:str):
+class Library(Group):
+    def __init__(self, lib_path:str, definition = None, name = None):
         self.lib_path = lib_path
+        # self.definition = definition
+        self.name = name
 
         # Don't know whether this should be a string or a list
         self.header_text = ""
@@ -34,18 +20,9 @@ class LibertyParser:
         # REMEMBER TO CLOSE THE FILE AT SOME POINT
         self.file = open(self.lib_path,'r')
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        # This needs to iterate to find each cell in chunks
-        self.cell = "Something needs go here"
-    
-    # Maybe put this in __init__?
-    def parse_header(self):
-        '''
-        Returns a Library Object with 'definition' as the header of the .lib file
-        '''
+        
+        # Returns a Library Object with 'definition' as the header of the .lib file
+        
         for line in self.file:
             # If not the first cell
             if not re.search('\s*cell\s*\(',line):
@@ -56,16 +33,20 @@ class LibertyParser:
         # (as blank lines will still have a newline character at the end, resulting in two lines being printed)                   
         if self.header_text.endswith("\n"):
             self.header_text = self.header_text[:-1]
-
-        return_object = Library(self.header_text)
         name_object = re.search(r'\((.*?)\)',self.header_text)       
 
-        return_object.name = name_object.group(1)
-        return_object.definition = self.header_text
-        return return_object
+        self.name = name_object.group(1)
+        self.definition = self.header_text
+        
+        
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # This needs to iterate to find each cell in chunks
+        self.cell = "Something needs go here"
     
-
     def get_cell(self,cell):
         '''
         Returns the names of the cells, the contents etc
@@ -76,11 +57,20 @@ class LibertyParser:
     def close_file(self):
         self.file.close()
 
+
+class Cell():
+    def __init__(self):
+        pass 
+
+class Pin():
+    def __init__(self):
+        pass   
+
+
 # Test code
-myFile = LibertyParser('example.lib')
-myObject = myFile.parse_header()
-print(myObject.name)
-print(myObject.definition)
+myFile = Library('example.lib')
+print(myFile.name)
+print(myFile.definition)
 myFile.close_file()
 
 
